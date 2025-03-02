b1eb85afa2d71fe8ff09e515c53e29505cbcdfed
Guo Yunhe <i@guoyunhe.me>
2025-03-02 19:18:15 +0800

    docs(readme): useRequireAuth()

--- README.md
+++ README.md
@@ -23,3 +23,40 @@ const App = () => {
   </AuthProvider>;
 };
 ```
+
+## API
+
+### `useRequireAuth(): (callback?: (error?: Error) => void) => Promise<void>`
+
+Return `requireAuth()` function, which can be called before user trigger some actions that require
+authentication, for example, like/dislike, comment, subscription...
+
+Callback style usage:
+
+```js
+const requireAuth = useRequireAuth();
+const handleLike = () => {
+  requireAuth((error) => {
+    if (error) {
+      console.error(error.message);
+    } else {
+      xior.post('/like');
+    }
+  });
+};
+```
+
+Promise style usage:
+
+```js
+const requireAuth = useRequireAuth();
+const handleLike = () => {
+  requireAuth()
+    .then(() => {
+      xior.post('/like');
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+};
+```
--- src/useRequireAuth.tsx
+++ src/useRequireAuth.tsx
@@ -6,18 +6,52 @@ import { useAuthStatus } from './useAuthStatus';
 
 /**
  * Return `requireAuth()` function, which can be called before user trigger some actions that require
- * authentication, for example, like/dislike, comment, read membership content...
+ * authentication, for example, like/dislike, comment, subscription...
+ *
+ * Callback style usage:
+ *
+ * ```js
+ * const requireAuth = useRequireAuth();
+ * const handleLike = () => {
+ *   requireAuth((error) => {
+ *     if (error) {
+ *       console.error(error.message);
+ *     } else {
+ *       xior.post('/like');
+ *     }
+ *   });
+ * };
+ * ```
+ *
+ * Promise style usage:
+ *
+ * ```js
+ * const requireAuth = useRequireAuth();
+ * const handleLike = () => {
+ *   requireAuth().then(() => {
+ *     xior.post('/like');
+ *   }).catch(error => {
+ *     console.error(error.message);
+ *   });
+ * };
+ * ```
  */
 export function useRequireAuth() {
   const { loginPath } = useAuth();
   const [location, navigate] = useLocation();
   const [status] = useAuthStatus();
 
-  const requireAuth = useLatestCallback((cb?: () => void) => {
+  const requireAuth = useLatestCallback(async (callback?: (error?: Error) => void) => {
     if (status === AuthStatus.NotLoggedIn) {
       navigate(loginPath, { state: { from: location } });
+      const e = new Error('Please login to continute');
+      if (callback) {
+        callback(e);
+      } else {
+        throw e;
+      }
     } else {
-      return cb?.();
+      callback?.();
     }
   });
 
